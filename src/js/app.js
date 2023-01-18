b581d4387d7c189cb03109550de134f8a88667e2
Carlo Autor <carloautor@gmail.com>
2023-01-18 20:53:58 +0800

    Added JS for monthly and yearly toggle at step 2
    Added JS for monthly and yearly toggle at step 2
    
    Working monthly and yearly toggle at step 2

--- src/js/app.js
+++ src/js/app.js
@@ -6,17 +6,30 @@ activeRadios.forEach((activeRadio) => {
 	});
 });
 
-let yearlyLabels = document.querySelectorAll(".form-group-label__yearly");
-let yearlySwitch = document.querySelector("#yearly");
+// let yearlyLabels = document.querySelectorAll(".form-group-label__yearly");
+// let yearlySwitch = document.querySelector("#yearly");
 
-yearlySwitch.addEventListener("click", () => {
-	yearlySwitch.toggleAttribute("checked");
-});
+// yearlySwitch.addEventListener("click", () => {
+// 	yearlySwitch.toggleAttribute("checked");
+// });
 
-yearlyLabels.forEach((yearlyLabel) => {
-	if (yearlySwitch.hasAttribute("checked")) {
-		yearlyLabel.style.display = "block";
-	} else {
-		yearlyLabel.style.display = "none";
-	}
-});
+// yearlyLabels.forEach((yearlyLabel) => {
+// 	if (yearlySwitch.hasAttribute("checked")) {
+// 		yearlyLabel.style.display = "block";
+// 	} else {
+// 		yearlyLabel.style.display = "none";
+// 	}
+// });
+
+const yearlyLabels = document.querySelectorAll(".form-group-label__yearly");
+const yearlySwitch = document.querySelector("#yearly");
+
+//add input eventhandler which calls the handleYearlyInput function
+yearlySwitch.addEventListener("input", handleYearlyInput);
+
+function handleYearlyInput(e) {
+	//for each label, set data atrribute 'show' with the value (true/false) of the switch input
+	yearlyLabels.forEach((yearlyLabel) => {
+		yearlyLabel.dataset.show = e.target.checked;
+	});
+}
